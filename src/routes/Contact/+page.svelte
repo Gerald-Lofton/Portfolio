30acbd49309c1fe7320d41aa9a4076cd66e05d14
Gerald-Lofton <geraldlofton0702@gmail.com>
2023-02-05 02:15:57 -0600

    More progress routes and view layouts

---
+++ src/routes/Contact/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<h1><b>Contact</b></h1>
+<br />
+<p>
+	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis cumque molestias voluptates eos
+	illum voluptas, perferendis sapiente recusandae exercitationem repudiandae. Quisquam provident et
+	tempore vero fugiat totam delectus at culpa?
+</p>
+<br />
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe rem vel cumque quia, explicabo at
+	enim temporibus, maiores iure voluptatibus eveniet similique porro assumenda voluptate qui maxime
+	sunt adipisci earum.
+</p>
